aead679a565713a30867a2747d79e5ca6ba78b07
moonpxi <paulo.schneider@gmail.com>
2013-05-13 21:34:20 +0100

    Adding jasmine for testing

---
+++ spec/js/TestSpec.js
@@ -0,0 +1,12 @@
+describe("Player", function() {
+  var something
+
+  beforeEach(function() {
+    something = "blah";
+  });
+
+  it("should test something", function() {
+    expect(something).toEqual("blah");
+  });
+
+});
