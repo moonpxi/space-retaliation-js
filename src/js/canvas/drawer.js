4060d9a154ad9a444a9c6593ff3bbdc7a7742bcb
moonpxi <paulo.schneider@gmail.com>
2013-05-19 12:28:23 +0100

    Setting up game loop and drawing moving rectangle

--- src/js/canvas/drawer.js
+++ src/js/canvas/drawer.js
@@ -1,4 +1,8 @@
-var Drawer = function(context) {
+var Drawer = function(canvas, context) {
+
+  this.clear = function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  };
 
   this.drawRect = function(color, x, y, width, height) {
     context.fillStyle = color;
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,25 @@
 window.onload = function() {
   var canvas = document.getElementById("game"),
       context = canvas.getContext("2d")
-      drawer = new Drawer(context);
+      drawer = new Drawer(canvas, context);
   
-  drawer.drawRect("#FF0000", 10, 10, 100, 200);
+  GameLoop(drawer, 30);
+}
+
+function GameLoop(drawer, fps) {
+  var x = 0;
+
+  this.loop = function() {
+    update();
+    draw();
+  };
+
+  this.update = function() {};
+  this.draw = function() {
+    drawer.clear();
+    drawer.drawRect("#FF0000", x, 10, 100, 200);
+    x += 1;
+  };
+
+  setInterval(loop, 1000/ fps);
 }
