2f36c363a987ceb44f3e22f51eb619c73b4c0705
moonpxi <paulo.schneider@gmail.com>
2013-05-19 12:30:47 +0100

    Setting up configuration

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,28 @@
+var Config = {
+  FPS: 30
+}
+
 window.onload = function() {
   var canvas = document.getElementById("game"),
       context = canvas.getContext("2d")
       drawer = new Drawer(canvas, context);
   
-  GameLoop(drawer, 30);
+  GameLoop(drawer, Config.FPS);
 }
 
 function GameLoop(drawer, fps) {
   var x = 0;
 
-  this.loop = function() {
-    update();
-    draw();
-  };
-
   this.update = function() {};
+
   this.draw = function() {
     drawer.clear();
     drawer.drawRect("#FF0000", x, 10, 100, 200);
     x += 1;
   };
 
-  setInterval(loop, 1000/ fps);
+  setInterval(function() {
+    update();
+    draw();
+  }, 1000/ fps);
 }
