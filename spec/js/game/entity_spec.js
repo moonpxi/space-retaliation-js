2c3e265100c7c975a0b2a901cf7330fcc45ef0ea
moonpxi <paulo.schneider@gmail.com>
2013-05-21 21:45:23 +0100

    Moving entity

--- spec/js/game/entity_spec.js
+++ spec/js/game/entity_spec.js
@@ -1,12 +1,21 @@
 describe("Entity", function() {
+  var entity;
 
-  it("have position and dimension", function() {
-    var entity = new Entity({x: 10, y: 20, width: 30, height: 40});
+  beforeEach(function() {
+    entity = new Entity({x: 10, y: 20, width: 30, height: 40});
+  });
 
+  it("has position and dimension", function() {
     expect(entity.x).toEqual(10);
     expect(entity.y).toEqual(20);
     expect(entity.width).toEqual(30);
     expect(entity.height).toEqual(40);
   });
 
+  it("moves relative to adjustment", function() {
+    entity.move(12, -5);
+
+    expect(entity.x).toEqual(22);
+    expect(entity.y).toEqual(15);
+  });
 });
--- src/js/game/entities.js
+++ src/js/game/entities.js
@@ -1,8 +1,15 @@
 var Entity = function(spec) {
   var that = {}
+
   that.x = spec.x;
   that.y = spec.y;
   that.width = spec.width;
   that.height = spec.height;
+  
+  that.move = function(dx, dy) {
+    that.x += dx;
+    that.y += dy;
+  }
+
   return that;
 };
